b798eeeca203f8267a208de2f9e86bc6cda04c55
Torrance Hodgson <torrance123@gmail.com>
2019-08-08 13:59:17 +0800

    Properly initialize p_i
    In the event that the list of DM halos is exhausted, p_i will be left
    at 0. This results in an invalid memory access and corruption of the
    cone array.

--- sampler/sampler.f90
+++ sampler/sampler.f90
@@ -1159,7 +1159,7 @@ program sampler
 
                     ! assign the galaxy to the closest halo mass in the mass bin
                     fom_old=abs(mgal-cone(istart_i,1)) !initialise distance betweem model mass and halo mass
-                    p_i=istart_i 
+                    p_i=istart_i+1
 
                     do iii=istart_i+1,iend_i
                        fom=abs(mgal-cone(iii,1))
@@ -1873,7 +1873,7 @@ program sampler
 !!$                    ! assign the galaxy to the closest halo mass in the mass bin
                        fom_old=abs(mgal-cone(istart_i,1)) !initialise distance betweem model mass and halo mass
                        ! associate galaxy to the closest available mass
-                       p_i=istart_i 
+                       p_i=istart_i+1
                        do iii=istart_i+1,iend_i
                           fom=abs(mgal-cone(iii,1))
                           if (fom<fom_old) then
